059c6afd07d6a50444f2c97847bdba599f899094
seanmcalinden <sean.f.mcalinden@live.co.uk>
2014-07-02 07:18:40 +0100

    Added new wait method

---
+++ Amido.WebDriver.Utilities.Tests/WaitExtensionTests.cs
@@ -0,0 +1,15 @@
+﻿using Amido.WebDriver.Utilities.Extensions;
+
+using NUnit.Framework;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Amido.WebDriver.Utilities.Tests
+{
+    [TestFixture]
+    public class WaitExtensionTests
+    {
+        
+    }
+}
--- Amido.WebDriver.Utilities/Extensions/WaitExtensions.cs
+++ Amido.WebDriver.Utilities/Extensions/WaitExtensions.cs
@@ -7,12 +7,36 @@ namespace Amido.WebDriver.Utilities.Extensions
 {
     public static class WaitExtensions
     {
-        public static void WaitForElement(IWebElement element, Func<IWebElement, bool> condition)
+        /// <summary>
+        /// Waits for an element.
+        /// </summary>
+        /// <param name="element">The element.</param>
+        /// <param name="condition">The condition.</param>
+        /// <param name="timeOutInSeconds">The time out in seconds.</param>
+        /// <param name="pollingIntervalInMilliseconds">The polling interval in milliseconds.</param>
+        public static void WaitForElement(IWebElement element, Func<IWebElement, bool> condition, int timeOutInSeconds = 10, int pollingIntervalInMilliseconds = 500)
         {
             IWait<IWebElement> wait = new DefaultWait<IWebElement>(element);
-            wait.Timeout = TimeSpan.FromSeconds(10);
-            wait.PollingInterval = TimeSpan.FromMilliseconds(500);
+            wait.Timeout = TimeSpan.FromSeconds(timeOutInSeconds);
+            wait.PollingInterval = TimeSpan.FromMilliseconds(pollingIntervalInMilliseconds);
             wait.Until(condition);
+        }
+
+        /// <summary>
+        /// Waits until a condition is met.
+        /// </summary>
+        /// <param name="element">The element.</param>
+        /// <param name="condition">The condition.</param>
+        /// <param name="timeOutInSeconds">The time out in seconds.</param>
+        /// <param name="pollingIntervalInMilliseconds">The polling interval in milliseconds.</param>
+        /// <returns></returns>
+        public static IWebElement WaitUntil(this IWebElement element, Func<IWebElement, bool> condition, int timeOutInSeconds = 10, int pollingIntervalInMilliseconds = 500)
+        {
+            IWait<IWebElement> wait = new DefaultWait<IWebElement>(element);
+            wait.Timeout = TimeSpan.FromSeconds(timeOutInSeconds);
+            wait.PollingInterval = TimeSpan.FromMilliseconds(pollingIntervalInMilliseconds);
+            wait.Until(condition);
+            return element;
         }
     }
 }
